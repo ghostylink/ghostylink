bbfc022bd50f72500f5adb9da90e72b74850a450
kevanescence <kevanescence@hotmail.fr>
2015-04-18 09:57:17 +0200

    FTest: add functional tests for link view and add

--- tests/TestCase/Selenium/LinkTest.php
+++ tests/TestCase/Selenium/LinkTest.php
@@ -4,11 +4,18 @@ class LinksTest extends PHPUnit_Extensions_SeleniumTestCase
   public $fixtureManager = null ;  
   public $autoFixtures = true ;
   public $dropTables = true;
+  
+  protected $captureScreenshotOnFailure = TRUE;
+  protected $screenshotPath = '/var/www/ghostylink_selenium/screenshots';
+  protected $screenshotUrl = 'http://kevin.remy.fr/ghostylink_selenium/screenshots';    
+  
   public $fixtures = [
         'Links' => 'app.links'
   ];
   protected function setUp()
   {
+    parent::setUp();
+    
     $this->fixtureManager = new Cake\TestSuite\Fixture\FixtureManager();
     $this->fixtureManager->fixturize($this);
     $this->fixtureManager->load($this);
@@ -21,13 +28,13 @@ class LinksTest extends PHPUnit_Extensions_SeleniumTestCase
     $this->open("/ghostylink/links/view/1");
     $this->chooseCancelOnNextConfirmation();
     $this->verifyTextPresent("Lorem ipsum dolor sit amet");
-    $this->verifyTextPresent("Lorem ipsum dolor sit amet, aliquet feugiat.");
+    $this->verifyTextPresent("qLorem ipsum dolor sit amet, aliquet feugiat.");
     $this->assertTrue($this->isElementPresent("css=a.delete-link"));
     $this->click("css=a.delete-link");
     $this->assertTrue((bool)preg_match('/^Are you sure you want to delete # 1[\s\S]$/',$this->getConfirmation()));
   }
   
-  public function testAdd() {
+  public function testAdd() {   
     $this->open("/ghostylink/links/add");
     $this->assertTrue($this->isElementPresent("css=form"));
     $this->assertTrue($this->isElementPresent("css=input[type=text]"));
