134e5cf02049987e65e0a8cdb3ed743312aa6df1
kevanescence <kevanescence@hotmail.fr>
2015-06-18 21:14:05 +0200

    Conf: update vendor directory

--- vendor/cakephp-plugins.php
+++ vendor/cakephp-plugins.php
@@ -2,10 +2,8 @@
 $baseDir = dirname(dirname(__FILE__));
 return [
     'plugins' => [
-        'AssetCompress' => $baseDir . '/vendor/markstory/asset_compress/',
         'Bake' => $baseDir . '/vendor/cakephp/bake/',
         'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
-        'MiniAsset' => $baseDir . '/vendor/markstory/mini-asset/'
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
     ]
 ];
