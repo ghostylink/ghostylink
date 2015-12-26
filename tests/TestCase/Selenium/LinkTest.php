933b28e64363c6a72d6da3d92c569879c43958d8
kevanescence <kevanescence@hotmail.fr>
2015-12-26 10:25:16 +0100

    FTest: link add test. Pass test when browser allow copy to clipboard funtionality. (eg FF 41+)

--- tests/TestCase/Selenium/LinkTest.php
+++ tests/TestCase/Selenium/LinkTest.php
@@ -46,7 +46,8 @@ class LinksTest extends FunctionalTest {
         $this->assertTrue($this->isElementPresent("css=section.generated-link"));
         // Click on the select button
         $this->click("css=button.link-copy");
-        $this->assertTextPresent("Press Ctlr+C to copy !");
+        $instructionPresent = $this->isTextPresent("Press Ctlr+C to copy !") || $this->isTextPresent("Copied !");
+        $this->assertTrue($instructionPresent, "Instruction is displayed when user click on the copy link button");
     }
 
     public function testAddComponentsWithSubmit() {
