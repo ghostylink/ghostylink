d1c7c18c30f8d66b55ea063448a6b2542ff69ffa
kremy <kevanescence@hotmail.fr>
2016-04-23 19:47:56 +0200

    FTest: link view test adaptation

---
+++ tests/TestCase/Selenium/Links/LinkViewTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LinkViewTest
+ *
+ * @author kremy
+ */
+class LinkViewTest extends FunctionalTest {
+
+    public function testStatisticsDisplayed()
+    {
+        $this->url("/a1d0c6e83f027327d8461063f4ac58a6");
+        $this->domChecker->assertTextNotPresent("Lorem ipsum dolor sit amet");
+        $this->domChecker->assertTextNotPresent("Lorem ipsum dolor sit amet, aliquet feugiat.");
+        $this->domChecker->clickOnElementMatching("css=button#load-link-max_views");
+        $this->domChecker->waitUntilTextPresent("Lorem ipsum dolor sit amet");
+        $this->domChecker->assertTextPresent("Lorem ipsum dolor sit amet, aliquet feugiat.");
+        
+        $this->domChecker->assertElementPresent("css=.link-stats");
+        $lpElem = $this->domChecker->findElementMatching("css=.link-remaining-views");
+        
+        debug($lpElem->text());
+        $this->assertRegExp("/^\d+ views left/", $lpElem->text());
+    }
+}
